ddab883940e15e1a1dcca24515fa16352ea8d3de
Dorsa Valli <dorsavali@yahoo.com>
2026-08-13 16:34:20 +0330

    home page half added

--- app/home.tsx
+++ app/home.tsx
@@ -1,19 +1,37 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
+import { Ionicons } from "@expo/vector-icons";
 const Home = () => {
-    const {username} = useLocalSearchParams();
-    return ( 
-        <SafeAreaProvider>
-            <SafeAreaView className="flex-1  bg-background p-6 w-full">
-                <Text className="font-poppins-semibold text-xl text-text-primary">
-                    Welcome Back, {username}!
-                </Text>
-                
-            </SafeAreaView>
-        </SafeAreaProvider>
-     );
-}
- 
+  const { username } = useLocalSearchParams();
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1  bg-background p-6 w-full gap-10">
+        <View className="flex-row justify-between gap-2">
+          <Text className="font-poppins-semibold text-xl text-text-primary">
+            Welcome Back, {username}!
+          </Text>
+          <Ionicons name="person-outline" size={24} color="#2E8B57" />
+        </View>
+        <View className="w-full  flex gap-3">
+          <View className="bg-white h-52 rounded-2xl  flex justify-center items-center gap-6">
+            <Text className="font-poppins-semibold text-xl text-text-secondary">Total Balance</Text>
+            <Text className="font-poppins-bold text-2xl">€12,540</Text>
+          </View>
+          <View className="flex-row justify-between gap-2">
+            <View className="bg-white w-1/2 flex rounded-2xl h-32 flex justify-center items-center gap-6">
+              <Text className="font-poppins-semibold text-lg text-success">Income</Text>
+              <Text className="font-poppins-bold text-xl">€19,200</Text>
+            </View>
+            <View className="bg-white w-1/2 flex rounded-2xl h-32 flex justify-center items-center gap-6">
+              <Text className="font-poppins-semibold text-lg text-danger">Expense</Text>
+              <Text className="font-poppins-bold text-xl">€6,660</Text>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
 export default Home;
