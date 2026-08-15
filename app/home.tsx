f49b7f1423b85e62aec7eaf81b0933213ec500b7
Dorsa Valli <dorsavali@yahoo.com>
2026-08-15 11:38:01 +0330

    cateories added

--- app/home.tsx
+++ app/home.tsx
@@ -2,11 +2,12 @@ import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
+import Categories from "../src/components/TempCategories";
 const Home = () => {
   const { username } = useLocalSearchParams();
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1  bg-background p-6 w-full gap-10">
+      <SafeAreaView className="flex-1  bg-background p-6 w-full gap-4">
         <View className="flex-row justify-between gap-2">
           <Text className="font-poppins-semibold text-xl text-text-primary">
             Welcome Back, {username}!
@@ -15,20 +16,27 @@ const Home = () => {
         </View>
         <View className="w-full  flex gap-3">
           <View className="bg-white h-52 rounded-2xl  flex justify-center items-center gap-6">
-            <Text className="font-poppins-semibold text-xl text-text-secondary">Total Balance</Text>
+            <Text className="font-poppins-semibold text-xl text-text-secondary">
+              Total Balance
+            </Text>
             <Text className="font-poppins-bold text-2xl">€12,540</Text>
           </View>
           <View className="flex-row justify-between gap-2">
             <View className="bg-white w-1/2 flex rounded-2xl h-32 flex justify-center items-center gap-6">
-              <Text className="font-poppins-semibold text-lg text-success">Income</Text>
+              <Text className="font-poppins-semibold text-lg text-success">
+                Income
+              </Text>
               <Text className="font-poppins-bold text-xl">€19,200</Text>
             </View>
             <View className="bg-white w-1/2 flex rounded-2xl h-32 flex justify-center items-center gap-6">
-              <Text className="font-poppins-semibold text-lg text-danger">Expense</Text>
+              <Text className="font-poppins-semibold text-lg text-danger">
+                Expense
+              </Text>
               <Text className="font-poppins-bold text-xl">€6,660</Text>
             </View>
           </View>
         </View>
+      <Categories />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ src/components/TempCategories.tsx
@@ -0,0 +1,67 @@
+import {  Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { ComponentProps } from "react";
+type IconName = ComponentProps<typeof Ionicons>["name"];
+type CategoryType = {
+    id: number;
+    title: string;
+    icon: IconName;
+    spend: string;
+    bg: string;
+}
+const Categories  = () => {
+    const data: CategoryType[]=[
+        {
+            id:1,
+            title : "Food",
+            icon : "fast-food-outline",
+            spend:"$200",
+            bg: "bg-[#FFB703]"
+        },
+        {
+            id:2,
+            title : "Transport",
+            icon : "car-outline",
+            spend:"$100",
+            bg: "bg-[#7BDFF2]"
+        },
+        {
+            id:3,
+            title : "Shopping",
+            icon : "cart-outline",
+            spend:"$300",
+            bg: "bg-[#F7A1C4]"
+        },
+        {
+            id:4,
+            title : "Bills",
+            icon : "cash-outline",
+            spend:"$400",
+            bg: "bg-[#F4D35E]"
+        }
+    ]
+    return ( 
+        <View className="flex justify-around gap-4">
+             <View className="flex-row justify-between items-center">
+                <Text className="font-poppins-semibold text-xl text-text-primary">
+                Categories
+            </Text>  
+            <Text className="font-poppins-semibold text-sm text-text-primary">
+                see all
+            </Text>  
+                </View>    
+            <View className="flex-row justify-between gap-2">
+                {data.map((item,index) => (
+                <View className="flex w-1/5 items-center gap-2 py-2 rounded-xl"  key={index}>
+                    <Ionicons name={item.icon} className={`p-2 rounded-2xl ${item.bg}`} size={30} />
+                    <Text className="font-poppins text-sm text-text-primary">{item.title}</Text>
+                    <Text className="font-poppins-semibold text-sm text-text-primary">{item.spend}</Text>
+                </View>
+            ))}
+            </View>
+            
+        </View>
+     );
+}
+ 
+export default Categories;
